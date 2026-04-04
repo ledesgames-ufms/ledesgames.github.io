54b976ee0f0331fa4a45e988edca810d5dcbd4ea
Arthur Barbosa <arthurbarbosaandrade2003@gmail.com>
2026-04-03 20:16:42 -0400

    fix: Remoção de dados repetidos dos testes e alteração na build para Actions

--- src/data/Publications.ts
+++ src/data/Publications.ts
@@ -19,35 +19,5 @@ export const publications: Publication[] = [
     date: "Maio 2025",
     type: "Artigo",
     link: "https://sol.sbc.org.br/index.php/sbsi/article/view/34347/34138"
-  },
-  {
-    id: 2,
-    title: "Pedagogical and Accessibility Guidelines for Digital Educational Games",
-    abstract: "The gaming industry in Brazil has grown progressively in recent years, accompanied by an increase in the demand for accessibility. However, the development of accessible products has not advanced at the same pace, resulting in a significant gap. Problems such as the lack of audio description and subtitles are critical barriers that affect the access, learning, and entertainment provided by digital games. These problems contribute to the social exclusion of people with disabilities.",
-    authors: "Gilvan Junior, Miguel de Albuquerque, Lucas Borth, Michele Soares, Awdren Fontão, Débora Paiva",
-    conference: "SBSI 2025",
-    date: "Maio 2025",
-    type: "Artigo",
-    link: "https://sol.sbc.org.br/index.php/sbsi/article/view/34347/34138"
-  },
-  {
-    id: 3,
-    title: "Pedagogical and Accessibility Guidelines for Digital Educational Games",
-    abstract: "The gaming industry in Brazil has grown progressively in recent years, accompanied by an increase in the demand for accessibility. However, the development of accessible products has not advanced at the same pace, resulting in a significant gap. Problems such as the lack of audio description and subtitles are critical barriers that affect the access, learning, and entertainment provided by digital games. These problems contribute to the social exclusion of people with disabilities.",
-    authors: "Gilvan Junior, Miguel de Albuquerque, Lucas Borth, Michele Soares, Awdren Fontão, Débora Paiva",
-    conference: "SBSI 2025",
-    date: "Maio 2025",
-    type: "Artigo",
-    link: "https://sol.sbc.org.br/index.php/sbsi/article/view/34347/34138"
-  },
-  {
-    id: 4,
-    title: "Pedagogical and Accessibility Guidelines for Digital Educational Games",
-    abstract: "The gaming industry in Brazil has grown progressively in recent years, accompanied by an increase in the demand for accessibility. However, the development of accessible products has not advanced at the same pace, resulting in a significant gap. Problems such as the lack of audio description and subtitles are critical barriers that affect the access, learning, and entertainment provided by digital games. These problems contribute to the social exclusion of people with disabilities.",
-    authors: "Gilvan Junior, Miguel de Albuquerque, Lucas Borth, Michele Soares, Awdren Fontão, Débora Paiva",
-    conference: "SBSI 2025",
-    date: "Maio 2025",
-    type: "Artigo",
-    link: "https://sol.sbc.org.br/index.php/sbsi/article/view/34347/34138"
-  },
+  }
 ];
